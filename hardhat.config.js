169904389bbefd0429f18e046b2981e7e4baa01a
Carlos Benjamin Alvarez Cruz <carlosalvarez098@gmail.com>
2022-02-18 14:53:34 -0600

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {version: '0.8.0'}, {version: '0.6.6'}
+    ]
+  }
+};
+
---
+++ test/sample-test.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("Gatekeeper Two", function () {
+  it("It should define entrant as the tx caller user1", async function () {
+
+  });
+});
